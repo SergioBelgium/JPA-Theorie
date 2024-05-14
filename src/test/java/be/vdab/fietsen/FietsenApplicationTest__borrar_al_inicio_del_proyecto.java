182e2f9103f3d76372131d8eae458d31b917b2d7
SergioBelgium <137648912+SergioBelgium@users.noreply.github.com>
2024-05-14 19:30:11 +0200

    eerste commit JPA ondersteuning

---
+++ src/test/java/be/vdab/fietsen/FietsenApplicationTest__borrar_al_inicio_del_proyecto.java
@@ -0,0 +1,13 @@
+package be.vdab.fietsen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FietsenApplicationTest__borrar_al_inicio_del_proyecto {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
