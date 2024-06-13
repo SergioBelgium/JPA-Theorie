424f54ad1cab3b2d762a47eca880d90a8d23afcb
SergioBelgium <137648912+SergioBelgium@users.noreply.github.com>
2024-06-13 12:51:00 +0200

    commit met Value Object

---
+++ src/main/java/be/vdab/fietsen/campussen/CampusController.java
@@ -0,0 +1,23 @@
+package be.vdab.fietsen.campussen;
+
+import org.aspectj.weaver.patterns.BindingAnnotationFieldTypePattern;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("campussen")
+
+class CampusController {
+    private final CampusService campusService;
+
+    public CampusController(CampusService campusService) {
+        this.campusService = campusService;
+    }
+    @GetMapping("westvlaams")
+    List<Campus> findWestVlaamse() {
+        return campusService.findWestVlaamse();
+    }
+}
